a431485db659424df38b19b1a2e3e347afadb697
Alex N <alex.n@starluxpro.com>
2024-11-19 16:32:41 -0500

    Added module to keep window state. Added parts required to use website icon. Added User Agent to fix issues with some sites.

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 const { app, BrowserWindow } = require('electron/main')
 const process = require('process')
 const fs = require('fs')
+const windowStateKeeper = require('electron-window-state');
 const config = require('./config.json')
 
 
 
-
-
 let url = config.URL
+let useragent = config.UserAgent
+
 
 const createWindow = () => {
+  // Load the previous state with fallback to defaults
+  let mainWindowState = windowStateKeeper({
+   defaultWidth: 1000,
+   defaultHeight: 800
+  });
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height
   })
 
+  mainWindowState.manage(win);
   win.removeMenu()
-  win.loadURL(url)
+  win.loadURL(url, {userAgent: useragent})
 }
 
 app.commandLine.appendSwitch("no-proxy-server")
@@ -30,9 +39,9 @@ app.whenReady().then(() => {
   })
 })
 
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
